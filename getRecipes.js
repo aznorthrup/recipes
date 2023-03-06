004f5ba7deaa2c5a96031fb13470b8f71feba5c9
aznorthrup <aznorthrup@gmail.com>
2023-03-05 20:15:50 -0700

    created cards for individual recipes

--- getRecipes.js
+++ getRecipes.js
@@ -17,15 +17,16 @@ const recipeType = document.getElementById("type").textContent
 let recipeList = ''
 for (let index=0; index<obj.recipes.length; index++) {
     if (recipeType == 'Breakfast' && obj.recipes[index].type == 'breakfast') {
-        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+        recipeList += '<div style="width:45%"><img src="https://user-images.githubusercontent.com/85071007/222539336-eda689ce-ce76-499d-ade8-843270058995.png" alt="'+obj.recipes[index].name+'" style="width:70%"><div><h4><b>'+obj.recipes[index].name+'</b></h4><p>'+obj.recipes[index].description+'</p></div></div>'
     } else if (recipeType == 'Lunch' && obj.recipes[index].type == 'lunch') {
-        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+        recipeList += '<div style="width:45%"><img src="https://user-images.githubusercontent.com/85071007/222539336-eda689ce-ce76-499d-ade8-843270058995.png" alt="'+obj.recipes[index].name+'" style="width:70%"><div><h4><b>'+obj.recipes[index].name+'</b></h4><p>'+obj.recipes[index].description+'</p></div></div>'
     } else if (recipeType == 'Dinner' && obj.recipes[index].type == 'dinner') {
-        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+        recipeList += '<div style="width:45%"><img src="https://user-images.githubusercontent.com/85071007/222539336-eda689ce-ce76-499d-ade8-843270058995.png" alt="'+obj.recipes[index].name+'" style="width:70%"><div><h4><b>'+obj.recipes[index].name+'</b></h4><p>'+obj.recipes[index].description+'</p></div></div>'
     } else if (recipeType == 'Dessert' && obj.recipes[index].type == 'dessert') {
-        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+        recipeList += '<div style="width:45%"><img src="https://user-images.githubusercontent.com/85071007/222539336-eda689ce-ce76-499d-ade8-843270058995.png" alt="'+obj.recipes[index].name+'" style="width:70%"><div><h4><b>'+obj.recipes[index].name+'</b></h4><p>'+obj.recipes[index].description+'</p></div></div>'
     } else if (recipeType == 'Other' && obj.recipes[index].type == 'other') {
-        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+        recipeList += '<div style="width:45%"><img src="https://user-images.githubusercontent.com/85071007/222539336-eda689ce-ce76-499d-ade8-843270058995.png" alt="'+obj.recipes[index].name+'" style="width:70%"><div><h4><b>'+obj.recipes[index].name+'</b></h4><p>'+obj.recipes[index].description+'</p></div></div>'
+    //   '<li>' + obj.recipes[index].name + '</li>'
     }
     document.getElementById("recipe").innerHTML = recipeList
 }
