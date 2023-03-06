296bbf0f311c5f5fc83ec9380795b2a83b9b80c2
aznorthrup <aznorthrup@gmail.com>
2023-03-05 19:31:57 -0700

    updated JS to filter recipes by type using JSON

--- getRecipes.js
+++ getRecipes.js
@@ -13,6 +13,19 @@ json += ']}'
 
 const obj = JSON.parse(json);
 
+const recipeType = document.getElementById("type").textContent
+let recipeList = ''
 for (let index=0; index<obj.recipes.length; index++) {
-    console.log(obj.recipes[index].name)
+    if (recipeType == 'Breakfast' && obj.recipes[index].type == 'breakfast') {
+        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+    } else if (recipeType == 'Lunch' && obj.recipes[index].type == 'lunch') {
+        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+    } else if (recipeType == 'Dinner' && obj.recipes[index].type == 'dinner') {
+        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+    } else if (recipeType == 'Dessert' && obj.recipes[index].type == 'dessert') {
+        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+    } else if (recipeType == 'Other' && obj.recipes[index].type == 'other') {
+        recipeList += '<li>' + obj.recipes[index].name + '</li>'
+    }
+    document.getElementById("recipe").innerHTML = recipeList
 }
